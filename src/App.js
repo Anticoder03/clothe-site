e3e397dd49e7472da48da3fa9337d83c37d349cd
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2025-05-10 13:10:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Main from './Main';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Routes>
+        {/* Define routes for each component */}
+        <Route path="/home" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <br />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from './components/Slider'
+import Seller from './components/Seller'
+
+export default function Main() {
+  return (
+    <>
+      <Slider />
+      <Seller />
+    </>
+  )
+}
